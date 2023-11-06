d173c4e38130441ced2f5f4dd17ed7621c5e84c8
Salman Arshad <salmanarshad90@gmail.com>
2023-11-06 14:00:00 +0000

    feat(layout): add header with profile dropdown and notifications

---
+++ src/layouts/full/header/Profile.js
@@ -0,0 +1,93 @@
+// import React, { useState } from 'react';
+// import { useNavigate } from 'react-router-dom';
+// import { Avatar, Box, Menu, Button, IconButton } from '@mui/material';
+
+const Profile = () => {
+  // const navigate = useNavigate();
+
+  // const [anchorEl2, setAnchorEl2] = useState(null);
+  // const handleClick2 = (event) => {
+  //   setAnchorEl2(event.currentTarget);
+  // };
+  // const handleClose2 = () => {
+  //   setAnchorEl2(null);
+  // };
+
+  // const handleLogout = () => {
+  //   localStorage.removeItem('authToken');
+  //   navigate('/auth/login');
+  // };
+
+  return (
+    // <Box>
+    //   <IconButton
+    //     size="large"
+    //     aria-label="show 11 new notifications"
+    //     color="inherit"
+    //     aria-controls="msgs-menu"
+    //     aria-haspopup="true"
+    //     sx={{
+    //       ...(typeof anchorEl2 === 'object' && {
+    //         color: 'primary.main',
+    //       }),
+    //     }}
+    //     onClick={handleClick2}
+    //   >
+    //     <Avatar
+    //       src={'/images/profile/Oval.png'}
+    //       alt={'/Oval.png'}
+    //       sx={{
+    //         width: 40,
+    //         height: 40,
+    //         // marginBottom: 0.5,
+    //       }}
+    //     />
+    //   </IconButton>
+    //   {/* ------------------------------------------- */}
+    //   {/* Message Dropdown */}
+    //   {/* ------------------------------------------- */}
+    //   <Menu
+    //     id="msgs-menu"
+    //     anchorEl={anchorEl2}
+    //     keepMounted
+    //     open={Boolean(anchorEl2)}
+    //     onClose={handleClose2}
+    //     anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+    //     transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+    //     sx={{
+    //       '& .MuiMenu-paper': {
+    //         width: '200px',
+    //       },
+    //     }}
+    //   >
+    //     {/* just commetning out for now  */}
+    //     {/* <MenuItem>
+    //       <ListItemIcon>
+    //         <IconUser width={20} />
+    //       </ListItemIcon>
+    //       <ListItemText>My Profile</ListItemText>
+    //     </MenuItem>
+    //     <MenuItem>
+    //       <ListItemIcon>
+    //         <IconMail width={20} />
+    //       </ListItemIcon>
+    //       <ListItemText>My Account</ListItemText>
+    //     </MenuItem>
+    //     <MenuItem>
+    //       <ListItemIcon>
+    //         <IconListCheck width={20} />
+    //       </ListItemIcon>
+    //       <ListItemText>My Tasks</ListItemText>
+    //     </MenuItem> */}
+    //     <Box mt={1} py={1} px={2}>
+    //       <Button onClick={handleLogout} variant="outlined" color="primary" fullWidth>
+    //         Logout
+    //       </Button>
+    //     </Box>
+    //   </Menu>
+    // </Box>
+    <></>
+  );
+};
+
+export default Profile;
